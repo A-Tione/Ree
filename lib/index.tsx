831abaf216328759ad6152eb173c3d24f2699254
Yang.he <yang.he@homepartners.tech>
2024-07-03 14:23:02 +0800

    feat: add html webpack-plugin

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,6 @@
-console.log('tset!!!');
+const div = document.createElement('div');
+div.innerHTML = `<h1>内容</h1>`;
+
+const root = document.getElementById('root') || document.body;
+console.log(root);
+root.appendChild(div);
