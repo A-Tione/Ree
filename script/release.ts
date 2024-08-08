e0664cc3d8ad6833b276adfa55097dc18793d9b6
a-tione <heyanggo@gmail.com>
2024-08-08 15:34:37 +0800

    feat: add ts-node

---
+++ script/release.ts
@@ -0,0 +1,21 @@
+const { execSync } = require("child_process");
+
+function runCommand(command: string) {
+  try {
+    execSync(command, { stdio: 'inherit' })
+  } catch (error) {
+    console.error(`Failed to execute command: ${command}`, error);
+    process.exit(1);
+  }
+}
+
+function release(versionType: 'patch' | 'minor' | 'major') {
+  runCommand(`npm version ${versionType}`);
+  runCommand('git commit -am "chore: bump version"');
+  runCommand('git push');
+}
+
+const args = process.argv.slice(2);
+console.log(1);
+
+release(args[0] as 'patch' | 'minor' | 'major' || 'patch');
