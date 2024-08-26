c5be4017e89a349b6dea3ca69e17028a348b4a05
a-tione <heyanggo@gmail.com>
2024-08-26 14:52:34 +0800

    chore: update doc.sh

--- doc.sh
+++ doc.sh
@@ -1,7 +1,8 @@
+current_branch=$(git rev-parse --abbrev-ref HEAD)
 yarn doc
 git checkout preview
-mv -f docs/* .
+mv -f docs/* ./
 git add .
 git commit -m 'update docs preview'
 git push
-git checkout -
+git checkout "$current_branch"
