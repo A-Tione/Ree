31c4c1539fc0ec8195869bc4e42209a848f6a7e8
a-tione <heyanggo@gmail.com>
2024-08-26 15:06:22 +0800

    update docs preview

--- doc.sh
+++ doc.sh
@@ -1,9 +1,10 @@
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-yarn doc
-git checkout preview
-echo "Current branch after checkout: $(git rev-parse --abbrev-ref HEAD)"
 mv -f docs/* ./
+yarn doc
 git add .
 git commit -m 'update docs preview'
+git push origin "$current_branch"
+git checkout preview
+git merge "$current_branch" -m 'merge'
 git push origin preview
 git checkout "$current_branch"
