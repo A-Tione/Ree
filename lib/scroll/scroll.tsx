cc0cb19b10abef6c0402a005da2c20199a2e9e9d
a-tione <heyanggo@gmail.com>
2024-09-10 15:36:33 +0800

    feat: add scroll bar and function

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,18 +1,102 @@
 import * as React from 'react';
-import { HTMLAttributes} from 'react'
+import { HTMLAttributes, useEffect, useRef, useState} from 'react'
 import './scroll.scss'
 import cachedScrollbarWidth from './scrollbar-width'
 
 interface Props extends HTMLAttributes<HTMLElement> {
-
 }
 
 const Scroll: React.FunctionComponent<Props> = (props) => {
   const {children, ...rest} = props;
+  const [barHeight, setBarHeight] = useState(0)
+  const [barTop, setBarTop] = useState(0)
+  const scrollRef = useRef<HTMLDivElement>(null)
+  const onScroll = (e: React.UIEvent) => {
+    const {current} = scrollRef
+    const scrollHeight = current!.scrollHeight
+    const viewHeight = current!.getBoundingClientRect().height
+    const scrollTop = current!.scrollTop
+    setBarTop((scrollTop * viewHeight) / scrollHeight)
+  }
+  const draggingRef = useRef(false)
+  const firstYRef = useRef(0)
+  const firstBarTopRef = useRef(0)
+  const onMouseDown = (e: React.MouseEvent) => {
+    draggingRef.current = true;
+    firstYRef.current = e.clientY
+    firstBarTopRef.current = barTop
+  }
+  
+  const onMouseMove = (e: MouseEvent) => {
+    if(draggingRef.current) {
+      const currentY = e.clientY - firstYRef.current
+      const newBarTop = firstBarTopRef.current + currentY
+      setBarTopHandle(newBarTop)
+      setScrollTop(newBarTop)
+    } 
+  }
+  const onMouseUp = () => {
+    draggingRef.current = false
+  }
+  const onselectstart = (e: Event) => {
+    if(draggingRef.current) {
+      e.preventDefault()
+    }
+  }
+  const onMouseDownTrack = (e: React.MouseEvent) => {
+    const newBarTop = e.clientY - (e.currentTarget.getBoundingClientRect().top + barHeight / 2)
+    setBarTopHandle(newBarTop)
+    setScrollTop(newBarTop)
+  }
+  const setBarTopHandle = (newBarTop: number) => {
+    if(newBarTop < 0) {return}
+    if(newBarTop + barHeight > scrollRef.current!.getBoundingClientRect().height) {return}
+    setBarTop(newBarTop)
+  }
+  const setScrollTop = (newBarTop: number) => {
+    const scrollHeight = scrollRef.current!.scrollHeight
+    const viewHeight = scrollRef.current!.getBoundingClientRect().height
+    scrollRef.current!.scrollTop = newBarTop * scrollHeight / viewHeight
+  }
+
+  
+  useEffect(() => {
+    document.addEventListener('mousemove', onMouseMove)
+    document.addEventListener('mouseup', onMouseUp)
+    document.addEventListener('selectstart', onselectstart)
+    return () => {
+      document.removeEventListener('mousemove', onMouseMove)
+      document.removeEventListener('mouseup', onMouseUp)
+      document.removeEventListener('selectstart', onselectstart)
+    }
+  })
+
+  useEffect(() => {
+    const {current} = scrollRef
+    const scrollHeight = current!.scrollHeight
+    const viewHeight = current!.getBoundingClientRect().height
+    const viewTop = current!.getBoundingClientRect().top
+
+    if(viewTop && viewHeight && scrollHeight) {
+      const barHeight = (viewHeight * viewHeight) / scrollHeight
+      setBarHeight(barHeight)
+    }
+  }, [])
+
   return (
     <div className='ree-scroll' {...rest}>
-      <div className='ree-scroll-inner' style={{right: -cachedScrollbarWidth()}}>
+      <div className='ree-scroll-inner' 
+          style={{right: -cachedScrollbarWidth()}}
+          onScroll={onScroll}
+          ref={scrollRef}
+      >
         {children}
+      </div>
+      <div className='ree-scroll-track' onMouseDown={onMouseDownTrack}>
+        <div className='ree-scroll-bar' 
+            style={{height: barHeight, transform: `translateY(${barTop}px)`}}
+            onMouseDown={onMouseDown}
+        />
       </div>
     </div>
   )
--- lib/scroll/scrollbar-width.tsx
+++ lib/scroll/scrollbar-width.tsx
@@ -1,50 +1,50 @@
-// let cachedScrollbarWidth: number | null = null;
-// let cachedDevicePixelRatio: number | null = null;
+let cachedScrollbarWidth: number | null = null;
+let cachedDevicePixelRatio: number | null = null;
 
-// window.addEventListener('resize', () => {
-//   if (cachedDevicePixelRatio !== window.devicePixelRatio) {
-//     cachedDevicePixelRatio = window.devicePixelRatio;
-//     cachedScrollbarWidth = null;
-//   }
-// });
+window.addEventListener('resize', () => {
+  if (cachedDevicePixelRatio !== window.devicePixelRatio) {
+    cachedDevicePixelRatio = window.devicePixelRatio;
+    cachedScrollbarWidth = null;
+  }
+});
 
-// export default function scrollbarWidth() {
-//   if (cachedScrollbarWidth === null) {
-//     if (typeof document === 'undefined') {
-//       cachedScrollbarWidth = 0;
-//       return cachedScrollbarWidth;
-//     }
+export default function scrollbarWidth() {
+  if (cachedScrollbarWidth === null) {
+    if (typeof document === 'undefined') {
+      cachedScrollbarWidth = 0;
+      return cachedScrollbarWidth;
+    }
 
-//     const body = document.body;
-//     const div = document.createElement('div');
+    const body = document.body;
+    const div = document.createElement('div');
 
-//     div.classList.add('ree-scroll-hide-scrollbar');
+    div.classList.add('ree-scroll-hide-scrollbar');
 
-//     body.appendChild(div);
+    body.appendChild(div);
 
-//     const width = div.getBoundingClientRect().right;
+    const width = div.getBoundingClientRect().right;
 
-//     // body.removeChild(div);
+    body.removeChild(div);
 
-//     cachedScrollbarWidth = width;
-//   }
-//   return cachedScrollbarWidth;
-// }
-
-export default function cachedScrollbarWidth() {
-
-  const div = document.createElement('div');
-
-  div.style.position = 'absolute';
-  div.style.top = div.style.left = '-9999px'; // 把 div 放到屏幕之外，防止影响用户
-  div.style.width = div.style.height = '100px';
-  div.style.overflow = 'scroll';
-
-  document.body.appendChild(div);
-
-  const width = div.offsetWidth - div.clientWidth;
-  console.log(width, 'width');
-  document.body.removeChild(div);
-
-  return width;
+    cachedScrollbarWidth = width;
+  }
+  return cachedScrollbarWidth || 17;
 }
+
+// export default function cachedScrollbarWidth() {
+
+//   const div = document.createElement('div');
+
+//   div.style.position = 'absolute';
+//   div.style.top = div.style.left = '-9999px'; // 把 div 放到屏幕之外，防止影响用户
+//   div.style.width = div.style.height = '100px';
+//   div.style.overflow = 'scroll';
+
+//   document.body.appendChild(div);
+
+//   const width = div.offsetWidth - div.clientWidth;
+//   console.log(width, 'width');
+//   document.body.removeChild(div);
+
+//   return width;
+// }
