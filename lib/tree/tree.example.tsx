28bcc56f50769dbc5dde27490846a4dbefc19ed1
a-tione <heyanggo@gmail.com>
2024-09-20 15:24:07 +0800

    feat: add checkbox

--- lib/tree/tree.example.tsx
+++ lib/tree/tree.example.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import Tree from './tree'
+import Tree, {SourceDataItem} from './tree'
 
 const TreeExample: React.FC = () => {
   const [array] = useState([
@@ -14,11 +14,23 @@ const TreeExample: React.FC = () => {
       {text: '2.1', value: '2.1'}
     ]}
   ])
+  const [selectedValues, setSelectedValues] = useState(['1.1.1'])
+  const onchange = (item: SourceDataItem, bool: boolean) => {
+    if (bool) {
+      setSelectedValues([...selectedValues, item.value])
+    } else {
+      setSelectedValues(selectedValues.filter(value => value !== item.value))
+    }
+  }
 
   return (
     <div>
       <h1>展示数据</h1>
-      <Tree sourceData={array}></Tree>
+      <Tree 
+        sourceData={array}
+        selectedValues={selectedValues} 
+        onChange={onchange}
+      />
     </div>
   )
 }
--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { scopedClassMaker } from '../helpers/classes';
 import './tree.scss'
 
-interface SourceDataItem {
+export interface SourceDataItem {
   text: string;
   value: string;
   children?: SourceDataItem[];
@@ -10,20 +10,35 @@ interface SourceDataItem {
 
 interface TreeProps {
   sourceData: SourceDataItem[]
+  selectedValues: string[];
+  onChange: (item: SourceDataItem, bool: boolean) => void
 }
 
 const scopedClass = scopedClassMaker('ree-tree')
 const sc = scopedClass
 
-const RenderItem: React.FC<{item: SourceDataItem; level?: number}> = ({item, level = 1}) => {
+const RenderItem: React.FC<{
+  item: SourceDataItem; 
+  onChange: (item: SourceDataItem, bool: boolean) => void;
+  selectedValues: string[], 
+  level?: number
+}> = ({item, onChange, selectedValues, level = 1}) => {
   const classes = {
     ['level-' + level]: true,
     'item': true
   }
+
   return <div className={sc(classes)}>
-    {item.text}
+    <div className={sc('text')}>
+      <input 
+        type='checkbox' 
+        onChange={ (e)=> onChange(item, e.target.checked)} 
+        checked={!!selectedValues.find(value => value === item.value)}
+      />
+      {item.text}
+    </div>
     {item.children?.map(item2 => {
-      return <RenderItem key={item2.value} item={item2} level={level + 1} />
+      return <RenderItem key={item2.value} item={item2} onChange={onChange} selectedValues={selectedValues} level={level + 1} />
     })}
   </div>
 }
@@ -32,7 +47,7 @@ const Tree: React.FC<TreeProps> = (props) => {
   return (
     <div>
       {props.sourceData.map(item => {
-        return <RenderItem key={item.value} item={item} />
+        return <RenderItem key={item.value} item={item} onChange={props.onChange} selectedValues={props.selectedValues} />
       })}
     </div>
   )
