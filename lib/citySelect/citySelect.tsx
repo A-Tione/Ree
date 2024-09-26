8b57f66bf42abd9f9cad87ef253423aae9d1e6fc
a-tione <heyanggo@gmail.com>
2024-09-26 11:54:46 +0800

    feat: add city data structure

--- lib/citySelect/citySelect.tsx
+++ lib/citySelect/citySelect.tsx
@@ -3,37 +3,44 @@ import './citySelect.scss';
 import { scopedClassMaker } from '@/helpers/classes';
 
 interface CitySelectProps {
-  cities: string[];
+  cities: { [province: string]: string[] };
   onChange: (city: string) => void;
 }
 
 const CitySelect: React.FC<CitySelectProps> = ({ cities, onChange }) => {
   const [selectedCity, setSelectedCity] = useState<string | null>(null);
-  const selectedCityClass = (city: string) => {
-    return {
-      'selected': selectedCity === city,
-      'item': true
-    }
-  }
   const sc = scopedClassMaker('ree-city-select');
+
   const handleCityClick = (city: string) => {
     setSelectedCity(city);
     onChange(city);
   };
 
+  const selectedClass = (item: string, selected: string | null) => {
+    return {
+      'selected': selected === item,
+      'item': true
+    }
+  }
+
   return (
     <div className={sc('')}>
-      <ul className={sc('list')}>
-        {cities.map((city) => (
-          <li
-            key={city}
-            className={sc(selectedCityClass(city))}
-            onClick={() => handleCityClick(city)}
-          >
-            {city}
-          </li>
-        ))}
-      </ul>
+      {Object.entries(cities).map(([province, citiesList]) => (
+        <div key={province} className={sc('province-container')}>
+          <div className={sc('province')}>{province}</div>
+          <div className={sc('cities')}>
+            {citiesList.map((city) => (
+              <div
+                key={city}
+                className={sc(selectedClass(city, selectedCity))}
+                onClick={() => handleCityClick(city)}
+              >
+                {city}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
